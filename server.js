fd1d460058f0169758c1b9f0cf2723f0354a6b9b
Sandeep Bassi <thecodingfiles@gmail.com>
2021-01-02 00:25:55 +0000

    added post route

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const path = require('path');
-const db = require('./db/db.json');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const db = require("./db/db.json");
+const fs = require("fs");
+const { nanoid } = require("nanoid");
 
 const app = express();
 const port = process.env.port || 8000;
@@ -11,18 +12,36 @@ app.use(express.json());
 // middleware to serve CSS files and JavaScript files in the public directory
 app.use(express.static("public"));
 
-// HTML routes
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
-app.get('*', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//______________API ROUTES________________
+app.post("/api/notes", (req, res) => {
+  // console.log('req body:', req.body);
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: nanoid(),
+  };
+  db.push(newNote);
+  // writes to db file
+  const file = path.join(__dirname, "./db/db.json");
+  fs.writeFile(file, JSON.stringify(db), (err) => {
+    if (err) throw err;
+    console.log("New note has been saved!");
+  });
+  res.send(newNote);
 });
 
-// API routes
-app.post('/api/notes', (req, res) => {
-    console.log('req body:', req.body);
+app.get("/api/notes", (req, res) => {
+  //console.log("db is:", db);
+  res.json(db);
 });
 
+//______________HTML ROUTES________________
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
 
-app.listen(port, () => console.log('listening on port:' , port));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => console.log("listening on port:", port));
