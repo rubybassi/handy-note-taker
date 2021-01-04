2eed007ec4d89aad9ebf5f320ff54ba4ce9105b2
Sandeep Bassi <thecodingfiles@gmail.com>
2021-01-04 02:41:53 +0000

    added delete function for delete route

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,18 @@ module.exports = (app) => {
   });
    
   // ROUTE - delete note
+  // In order to delete a note, you'll need to read all notes from the `db.json` file, 
+  // remove the note with the given `id` property, and then rewrite the notes to the `db.json` file. 
   app.delete("/api/notes/:id", (req, res) => {
+    // console.log(req.params.id);
+    const id = req.params.id;
+    deleteNote(id).then((data) => res.json(data)).catch((err) => console.log(err));
+
+  });
+
+
+
+  /*app.delete("/api/notes/:id", (req, res) => {
     // console.log(req.params.id);
     const id = req.params.id;
     db = db.filter((note) => id !== note.id);
@@ -32,7 +43,7 @@ module.exports = (app) => {
       console.log("New note has been saved!");
     });
     res.end();
-  });
+  });*/
 
   // Function for retrieving notes - set as async to allow promise chaining when called
   const retrieveNotes = async () => {
@@ -43,7 +54,7 @@ module.exports = (app) => {
   };
 
   // Function for pushing new note and writing to db
-  const writeNote = async (note) => {
+  const writeNote = async note => {
     const allNotes = await retrieveNotes();
     //console.log('allNotes:', allNotes);
     allNotes.push(note);
@@ -51,4 +62,13 @@ module.exports = (app) => {
     await fs.writeFile(file, JSON.stringify(allNotes));
     return note;
   };
+
+  // Function for filtering deleted note by id, writing to db and returning updated notes
+  const deleteNote = async (id) => {
+    const allNotes = await retrieveNotes();
+    const filteredNotes = await allNotes.filter((note) => id !== note.id);
+    const file = path.join(__dirname, "../db/db.json");
+    await fs.writeFile(file, JSON.stringify(filteredNotes));
+    return updatedNotes = retrieveNotes();
+  }
 };
