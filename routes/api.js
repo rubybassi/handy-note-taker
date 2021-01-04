98cc0bb099e43e51b7a7d8ed61b63e10d8eaab43
Sandeep Bassi <thecodingfiles@gmail.com>
2021-01-04 02:22:54 +0000

    added write note function for post route

--- routes/api.js
+++ routes/api.js
@@ -3,33 +3,24 @@ let db = require("../db/db.json");
 const fs = require("fs").promises;
 const { nanoid } = require("nanoid");
 
-module.exports = app => {
-  // send notes
+module.exports = (app) => {
+
+  // ROUTE - send notes
   app.get("/api/notes", (req, res) => {
-    //console.log("request object:", req);
-    //const notes = retrieveNotes();
-    retrieveNotes().then(data => res.json(data)).catch(err => console.log(err));
-    //res.json(notes);
+    retrieveNotes().then(data => res.json(data)).catch((err) => console.log(err));
   });
 
-  // recieve new note
+  // ROUTE - recieve new note
   app.post("/api/notes", (req, res) => {
     const newNote = {
       title: req.body.title,
       text: req.body.text,
       id: nanoid(),
     };
-    db.push(newNote);
-    // writes to db file
-    const file = path.join(__dirname, "../db/db.json");
-    fs.writeFile(file, JSON.stringify(db), (err) => {
-      if (err) throw err;
-      console.log("New note has been saved!");
-    });
-    res.send(newNote);
+    writeNote(newNote).then(data => res.json(data)).catch((err) => console.log(err));
   });
-
-  // delete note
+   
+  // ROUTE - delete note
   app.delete("/api/notes/:id", (req, res) => {
     // console.log(req.params.id);
     const id = req.params.id;
@@ -43,12 +34,21 @@ module.exports = app => {
     res.end();
   });
 
-  // universal function for retrieving notes - set as async to allow promise chaining when called
+  // Function for retrieving notes - set as async to allow promise chaining when called
   const retrieveNotes = async () => {
     const file = path.join(__dirname, "../db/db.json");
     const data = await fs.readFile(file);
     const notes = JSON.parse(data);
     return notes;
   };
-};
 
+  // Function for pushing new note and writing to db
+  const writeNote = async (note) => {
+    const allNotes = await retrieveNotes();
+    //console.log('allNotes:', allNotes);
+    allNotes.push(note);
+    const file = path.join(__dirname, "../db/db.json");
+    await fs.writeFile(file, JSON.stringify(allNotes));
+    return note;
+  };
+};
